78a65a4364bf49c2efb977f5b80074825f6ed3ba
Roman <roman@Romans-MacBook-Pro.local>
2021-07-18 00:29:30 +0300

    Test Push

--- resources/views/myadmin/main.blade.php
+++ resources/views/myadmin/main.blade.php
@@ -3,9 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>@yield('head-title')</title>
-        <link rel="icon"
-              type="image/svg"
-              href="{{ asset('public/includes/images/favicon.svg') }}">
+        <link rel="icon" type="image/svg" href="{{ asset('public/includes/images/favicon.svg') }}">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Product+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('public/includes/front/css/admin-bar.css') }}">
